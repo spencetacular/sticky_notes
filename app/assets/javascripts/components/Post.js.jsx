d4cd36c273546e3153991a7c0ba9634b567fcc02
Sam Hammer <sam@michaelrobertson.com>
2016-01-04 16:35:25 -0700

    added form

--- app/assets/javascripts/components/Post.js.jsx
+++ app/assets/javascripts/components/Post.js.jsx
@@ -14,14 +14,21 @@ var Post = React.createClass({
 	submitPost: function(e){
 		e.preventDefault();
 		var self = this;
+		var name = this.state.postName;
+		var description = this.state.postDescription;
+		var priority = this.state.postPriority;
 		$.ajax({
-			url: 'posts',
+			url: '/notes',
 			type: 'POST',
-			data: {post: {name: this.state.itemName}},
+			data: {post: {name: name, description: description, priority: priority}},
 			success: function(item){
-				var items = self.state.items;
-				items.push({name: item.name, complete: item.complete});
-				self.setState({items: items, showAdd: false, itemName: null});
+				var items = self.state.post;
+				// var items = self.state.posts;
+				items.push({name: item.postName, description: item.postDescription, priority: item.postPriority});
+				// self.setState({items: items, showAdd: false, itemName: null});
+			},
+			error: function() {
+				debugger
 			}
 		});
 	},
@@ -39,6 +46,32 @@ var Post = React.createClass({
 
 	},
 
+	deleteNote: function(e){
+		debugger
+	},
+
+	displayPosts: function() {
+	
+		var posts = [];
+		for(var i=0; i<this.state.post.length; i++) {
+			posts.push(<div className='card'>
+										<div className='card-content'>
+											<p> Title: </p>
+											{this.state.post[i].name}
+											<br/>
+											<br/>
+											{this.state.post[i].description}
+											<p> Priority: </p>
+											{this.state.post[i].priority}
+											<br/>
+											<button onClick={deleteNote()}> Remove </button>
+										</div>
+									</div>
+				);
+		};
+		return posts;
+	},
+
 	render: function(){
 		return (<div>
 							<form onSubmit={this.submitPost}>
@@ -51,12 +84,11 @@ var Post = React.createClass({
 								<p className='range-field'>
 									<input type='range' min='0' max='10' onChange={this.addPostPriority}/>
 								</p>
-								<a className ='waves-effect waves-light btn' type='submit'>Create Post it</a>
+								<button className ='waves-effect waves-light btn' type='submit'>Create Post it</button>
 							</form>	
+						 {this.displayPosts()}
 						</div>
-
 			);
-
 	}
 
 });
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,19 @@ class NotesController < ApplicationController
   	@posts = Post.all
   end
 
+  # def new
+  #   @post = Post.new
+  # end
+
   def create
+    post = Post.create(note_params)
+    render json: post
   end
+
+  private
+
+  def note_params
+    params.require(:post).permit(:name, :description, :priority)
+  end
+
 end
